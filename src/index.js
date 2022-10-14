be1e907076d59ce9c917d61a5f8a054043e5fbf6
BELAIDYasser <yasser.bel93@gmail.com>
2022-10-14 22:44:51 +0200

    integrate the header component & global css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
     <Footer/>
   </React.StrictMode>
 );
