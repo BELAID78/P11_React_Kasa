60c07e5f67ac811981148732d4acd0e04186fce4
BELAIDYasser <yasser.bel93@gmail.com>
2022-10-15 08:12:03 +0200

    integrate the slider component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
