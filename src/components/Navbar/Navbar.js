d8d7022600efe83cedff07ab48a1693b1a4ad6ed
BELAIDYasser <yasser.bel93@gmail.com>
2022-10-16 17:39:00 +0200

    integrate the detail page & edit style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,30 @@ const Navbar = ({children}) => {
             <div className='navbar-content'>
                 {children}
             </div>
+        </navimport { useNavigate } from 'react-router-dom';
+import navBarLogo from './../../images/navbar_logo.svg';
+import './navbar.scss';
+
+const Navbar = ({children}) => {
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/');
+    };
+
+    return (
+        <nav className='navbar'>
+            <div onClick={() => navigateToHome()} className='navbar-logo'>
+                <img alt='kasa project logo' src={navBarLogo} />
+            </div>
+            <div className='navbar-content'>
+                {children}
+            </div>
         </nav>
+    )
+}
+
+export default Navbar;
     )
 }
 
